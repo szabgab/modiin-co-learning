9f0554b3235d53030059fee4aced40cd03d4035f
Gabor Szabo <gabor@szabgab.com>
2017-03-07 07:34:43 +0200

    check required fields

--- test.py
+++ test.py
@@ -2,8 +2,14 @@ import json
 
 filename = 'participants.json'
 try:
-    json.load(open(filename))
+    people = json.load(open(filename))
 except Exception as e:
     print("Could not read {}".format(filename))
     print(e)
     exit(1)
+
+for p in people:
+    for field in ['name', 'github']:
+        if field not in p:
+            exit("'{}' is missing from {}".format(field, str(p)))
+
